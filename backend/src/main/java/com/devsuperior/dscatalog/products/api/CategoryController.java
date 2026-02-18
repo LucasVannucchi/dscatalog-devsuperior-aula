fb5e465033048e92b749c74a4bc2cea29ddcf9ff
Lucas Vannucchi <lucasvannucchi16@gmail.com>
2026-02-18 15:28:11 -0300

    Update Category

--- backend/src/main/java/com/devsuperior/dscatalog/products/api/CategoryController.java
+++ backend/src/main/java/com/devsuperior/dscatalog/products/api/CategoryController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,6 +48,14 @@ public class CategoryController {
     public ResponseEntity<CategoryResponseDTO> findById(@PathVariable Long id) {
         CategoryResponseDTO dto = categoryService.findById(id);
         return ResponseEntity.ok().body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<CategoryResponseDTO> update(
+            @PathVariable("id") Long id,
+            @RequestBody CategoryRequestDTO dto){
+        CategoryResponseDTO response = categoryService.update(id, dto);
+        return ResponseEntity.ok().body(response);
     }
 
 }
--- backend/src/main/java/com/devsuperior/dscatalog/products/domain/service/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/products/domain/service/CategoryService.java
@@ -3,10 +3,10 @@ package com.devsuperior.dscatalog.products.domain.service;
 import com.devsuperior.dscatalog.products.domain.dto.CategoryRequestDTO;
 import com.devsuperior.dscatalog.products.domain.dto.CategoryResponseDTO;
 import com.devsuperior.dscatalog.products.domain.entity.Category;
-import com.devsuperior.dscatalog.products.domain.exceptions.EntityNotFoundException;
+import com.devsuperior.dscatalog.products.domain.exceptions.ResourceNotFoundException;
 import com.devsuperior.dscatalog.products.domain.mapper.CategoryMapper;
 import com.devsuperior.dscatalog.products.repository.CategoryRepository;
-import jakarta.validation.Valid;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -38,14 +38,26 @@ public class CategoryService {
     public CategoryResponseDTO findById(Long id) {
         Optional<Category> cat = categoryRepository.findById(id);
         Category entity = cat.orElseThrow(() ->
-                new EntityNotFoundException("Category not found with ID " + id));
+                new ResourceNotFoundException("Category not found with Id: " + id));
         return new CategoryResponseDTO(entity);
     }
 
     @Transactional
-    public CategoryResponseDTO insert(@Valid CategoryRequestDTO dto) {
+    public CategoryResponseDTO insert(CategoryRequestDTO dto) {
         Category category = categoryMapper.toEntity(dto);
         category = categoryRepository.save(category);
         return categoryMapper.toDTO(category);
+    }
+
+    @Transactional
+    public CategoryResponseDTO update(Long id, CategoryRequestDTO dto) {
+        try {
+            Category category = categoryRepository.getReferenceById(id);
+            category.setName(dto.name());
+            category = categoryRepository.save(category);
+            return categoryMapper.toDTO(category);
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException("Id: " + id + " not found");
+        }
     }
 }
